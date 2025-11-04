e8bb61fb9a1c3808e860fc6f079f867c8f84e5d7
肆鸠 <si_jiu49@outlook.com>
2025-11-04 19:42:07 +0800

    feat: 音乐播放器进度条

--- assets/js/musicPlayer.js
+++ assets/js/musicPlayer.js
@@ -1,7 +1,6 @@
 let playList, playingIndex = 0, playingNow, musicStop = 0;
 
-const loadPlayer = async () => {
-    const server = "netease", type = "playlist", id = 14175652304;
+const loadPlayer = async (server, type, id) => {
     const api = `https://api.i-meto.com/meting/api?server=${server}&type=${type}&id=${id}&r=${Math.random()}`;
     const res = await fetch(api);
     res.json().then((data) => {
@@ -12,24 +11,36 @@ const loadPlayer = async () => {
 }
 
 function playMusic() {
-    const music = playList[playingIndex];
+    const music = playList[playingIndex], musicProgress = document.getElementById("musicProgress");
     playingNow = new Audio(music?.url);
     playingNow.play();
-    // playingNow.addEventListener("loadedmetadata", () => {
-    // });
     document.getElementById("cover").setAttribute("src", music?.pic);
     document.getElementById("musicTitle").textContent = music?.title;
     document.getElementById("musicAuthor").textContent = music?.author;
+    playingNow.addEventListener("loadedmetadata", () => {
+        // console.log(playingNow.duration);
+        musicProgress.setAttribute("max", playingNow.duration);
+        playingNow.addEventListener("timeupdate", () => {
+            musicProgress.setAttribute("value", playingNow.currentTime);
+        });
+        playingNow.addEventListener("ended", () => {
+            nextMusic();
+            return;
+        });
+    });
+
 }
 
 function preMusic() {
     playingNow.pause();
+    playingNow = null;
     playingIndex--;
     if (playingIndex < 0) {
         playingIndex = playList.length - 1;
     }
     playMusic();
 }
+
 function stopStartMusic() {
     musicStop = !musicStop;
     if (musicStop) {
@@ -44,6 +55,7 @@ function stopStartMusic() {
 
 function nextMusic() {
     playingNow.pause();
+    playingNow = null;
     playingIndex++;
     if (playingIndex >= playList.length) {
         playingIndex = 0;
@@ -52,4 +64,4 @@ function nextMusic() {
 }
 
 
-loadPlayer();
+loadPlayer("netease", "playlist", 14175652304);
